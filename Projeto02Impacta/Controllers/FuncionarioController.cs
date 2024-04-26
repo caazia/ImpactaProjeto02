219e0756c3eee70afc04219b7ef108b06d4d6b87
caazia <contato.cassia.carvalho@gmail.com>
2024-04-26 15:50:08 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto02Impacta/Controllers/FuncionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto02Impacta.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
